3ffd755898435cab0e569dcdb07fe8e08d191f83
Seppe Lesschaeve <lesschaeveseppe@gmail.com>
2021-12-02 15:09:38 +0100

    Day 2

---
+++ day2-1.hs
@@ -0,0 +1,26 @@
+import System.IO  
+import Control.Monad
+
+getContent :: String -> [String]
+getContent s = getContentHelper (lines s)
+
+getContentHelper :: [String] -> [String]
+getContentHelper [] = []
+getContentHelper (content:contents) = (words content) ++ (getContentHelper contents)
+
+readAmount :: String -> Int
+readAmount = read
+
+getMultipleHD :: [String] -> Int -> Int -> Int
+getMultipleHD [] hor depth =  hor * depth
+getMultipleHD (dir:am:xs) hor depth
+  | dir == "forward" = getMultipleHD xs (hor + (readAmount am)) depth
+  | dir == "down"    = getMultipleHD xs hor (depth + (readAmount am))
+  | dir == "up"      = getMultipleHD xs hor (depth - (readAmount am))
+
+
+main :: IO Int
+main = do
+  myFile <- openFile "input2.txt" ReadMode
+  content <- hGetContents myFile
+  return (getMultipleHD (getContent content) 0 0)
---
+++ day2-2.hs
@@ -0,0 +1,26 @@
+import System.IO  
+import Control.Monad
+
+getContent :: String -> [String]
+getContent s = getContentHelper (lines s)
+
+getContentHelper :: [String] -> [String]
+getContentHelper [] = []
+getContentHelper (content:contents) = (words content) ++ (getContentHelper contents)
+
+readAmount :: String -> Int
+readAmount = read
+
+getMultipleHD :: [String] -> Int -> Int -> Int -> Int
+getMultipleHD [] hor depth aim =  hor * depth
+getMultipleHD (dir:am:xs) hor depth aim
+  | dir == "forward" = getMultipleHD xs (hor + (readAmount am)) (depth + (aim * (readAmount am))) aim
+  | dir == "down"    = getMultipleHD xs hor depth (aim + (readAmount am))
+  | dir == "up"      = getMultipleHD xs hor depth (aim - (readAmount am))
+
+
+main :: IO Int
+main = do
+  myFile <- openFile "input2.txt" ReadMode
+  content <- hGetContents myFile
+  return (getMultipleHD (getContent content) 0 0 0)
