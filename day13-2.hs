4e5a1a84648f13e10fe6c0ad29e08d3242a655d9
Seppe Lesschaeve <lesschaeveseppe@gmail.com>
2021-12-13 08:24:23 +0100

    Day 13 clean

--- day13-2.hs
+++ day13-2.hs
@@ -48,9 +48,42 @@ getDotsAfterInstructions :: ([(Int,Int)], [(Int,Int)]) -> [(Int,Int)]
 getDotsAfterInstructions (dots, []) = dots
 getDotsAfterInstructions (dots, (instruction:instructions)) = getDotsAfterInstructions (newDots, instructions)
   where newDots = getDotsAfterInstruction (dots, instruction)
+  
+fillRow :: String -> (Int,Int) -> Int -> String
+fillRow [] _ _ = []
+fillRow (dot:dots) (dotX, dotY) colIndex
+  | colIndex == dotX = '*':dots
+  | otherwise        = dot:(fillRow dots (dotX,dotY) (colIndex + 1))
 
-main :: IO [(Int,Int)]
+fillMatrix :: [String] -> (Int, Int) -> Int -> [String] 
+fillMatrix [] _ _ = [] 
+fillMatrix (rowOfDots:rowsOfDots) (dotX, dotY) rowIndex 
+  | rowIndex == dotY = (fillRow rowOfDots (dotX, dotY) 0):rowsOfDots
+  | otherwise        = rowOfDots:(fillMatrix rowsOfDots (dotX, dotY) (rowIndex + 1))      
+  
+fillAllDots :: [(Int,Int)] -> [String] -> [String]
+fillAllDots [] dots = dots
+fillAllDots (dot:dots) dotMatrix = fillAllDots dots newDotMatrix
+  where newDotMatrix = fillMatrix dotMatrix dot 0  
+  
+emptyMatrix :: Int -> Int -> [String]
+emptyMatrix xMax yMax = replicate (yMax+1) (replicate (xMax+1) ' ')
+
+getMatrixMeasures :: [(Int,Int)] -> (Int,Int) -> (Int,Int)
+getMatrixMeasures [] measures = measures
+getMatrixMeasures ((dotX,dotY):dots) (curMaxX, curMaxY) = getMatrixMeasures dots ((maximum [dotX, curMaxX]),(maximum [dotY, curMaxY])) 
+
+getFilledMatrix :: [(Int,Int)] -> [String]
+getFilledMatrix dots = fillAllDots dots beginMatrix
+  where 
+    (xMax, yMax) = getMatrixMeasures dots (0,0)
+    beginMatrix  = emptyMatrix xMax yMax
+
+printDots :: [(Int,Int)] -> IO ()
+printDots dots = mapM_ putStrLn (getFilledMatrix dots)
+    
+main :: IO ()
 main = do
   myFile <- openFile "input13.txt" ReadMode
   content <- hGetContents myFile
-  return (getDotsAfterInstructions (getInputDots content))
+  printDots (getDotsAfterInstructions (getInputDots content))
