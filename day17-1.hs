8a6ff1e8fddf19d93fe0a0ae22624a744c66288d
Seppe Lesschaeve <lesschaeveseppe@gmail.com>
2021-12-17 10:03:26 +0100

    Day 17

---
+++ day17-1.hs
@@ -0,0 +1,44 @@
+import System.IO  
+import Control.Monad
+import Data.List.Split
+
+updateDX :: Int -> Int 
+updateDX dx
+  | dx > 0 = dx - 1
+  | dx < 0 = dx + 1
+  | otherwise  = dx
+
+step :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+step (x,y) (dx, dy) = (x, y) : step (x + dx, y + dy) ((updateDX dx), dy - 1) 
+
+isTarget :: Int -> Int -> (Int, Int) -> (Int, Int) -> Bool
+isTarget x y (xmin, xmax) (ymin, ymax) = xmin <= x && x <= xmax && ymin <= y && y <= ymax
+ 
+getTrajectory ymin = takeWhile (\(x, y) -> y >= ymin) . step (0, 0)
+isValid trajectory (xmin, xmax) (ymin, ymax) = any (\(x, y) -> isTarget x y (xmin, xmax) (ymin, ymax)) trajectory
+
+allTrajectories (xmin, xmax) (ymin, ymax) =
+  [ trajectory | dx <- [1 .. xmax] , dy <- [ymin .. negate ymin] , let trajectory = getTrajectory ymin (dx, dy), isValid trajectory (xmin, xmax) (ymin, ymax)]
+
+maxY :: ((Int, Int),(Int, Int)) -> Int
+maxY ((xmin, xmax), (ymin, ymax)) = maximum . map snd . concat $ allTrajectories (xmin, xmax) (ymin, ymax)
+
+getBounds :: String -> (Int, Int)
+getBounds s = (read (boundsStrings!!0), read (boundsStrings!!1))
+  where 
+    bounds = tail ( tail ( s ) )
+    boundsStrings = splitOn ".." bounds 
+
+getTargetArea :: String -> ((Int,Int),(Int, Int))
+getTargetArea s = (xBounds, yBounds)
+  where 
+    spaceWords = words s
+    xBounds = getBounds (head (splitOn "," (spaceWords!!2)))
+    yBounds = getBounds (spaceWords!!3)
+  
+main :: IO Int
+main = do
+  myFile <- openFile "input17.txt" ReadMode
+  content <- hGetContents myFile
+  return (maxY (getTargetArea content))
+  
